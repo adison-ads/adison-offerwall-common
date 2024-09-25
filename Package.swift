27a728bdce27af4a99c978c4bdbc5875a49e7fae
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-09-25 14:47:42 +0900

    0.9.0-beta1 릴리즈

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdisonOfferwallCommon",
-            url: "https://github.com/adison-ads/adison-offerwall-common",
-            checksum: ""
+            url: "https://github.com/adison-ads/adison-offerwall-common/releases/download/0.9.0-beta1/AdisonOfferwallCommon.zip",
+            checksum: "7ca60ad59d1564305a38029fa9da99e4d776ac72b6b203f63fc5121df5cf043d"
         )
     ]
 )
