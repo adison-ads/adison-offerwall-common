2856d95568d528d6758f8bda1f77a5d666f71fd4
Bitrise CI <noreply@bitrise.io>
2026-03-10 08:53:21 +0000

    1.1.0 릴리즈

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "1.0.2"
-let checksum = "8da12645baa562e84a15276587e5957e61d87171676af0cc9477327a056b3fad"
+let version = "1.1.0"
+let checksum = "43dedd689c14b81a65f58c7814b5dcbfcfd062ac6a9a677ee9ede698271421dd"
 
 let package = Package(
     name: "AdisonOfferwallCommon",
